5bb0a61bf5d9155b085f04261d543e500a755124
ishaanjain047 <83901051+ishaanjain047@users.noreply.github.com>
2022-10-26 17:52:06 +0530

    carousel fixed

--- src/components/Home/EventsHome.js
+++ src/components/Home/EventsHome.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
-import Event1 from "../../assets/Event1.png";
+import Event1b from "../../assets/Event1.png";
 import Event2 from "../../assets/Event2.png";
 import Event3 from "../../assets/Event3.png";
 import Event4 from "../../assets/Event4.png";
+import Event5 from "../../assets/Event5.png";
+import Event6 from "../../assets/Event6.png";
 import "./EventsHome.css";
 import prevbtn from "../../assets/prevbtn.png";
 import nextbtn from "../../assets/nextbtn.png";
@@ -54,26 +56,28 @@ function Events() {
       <div className="eventHomeContainer">
         <section>
           <div className="sec1">
-            <img className="eventImages" src={Event1} alt="/" />
-            <div className="eventName event1">Event1</div>
+            <img className="eventImages" src={Event1b} alt="/" />
+            <div className="eventName event1">• Case Study</div>
           </div>
           <div className="sec2">
             <img className="eventImages" src={Event2} alt="/" />
-            <div className="eventName event2">Event2</div>
+            <div className="eventName event2">• Research Work</div>
           </div>
           <div className="sec3">
             <img className="eventImages" src={Event3} alt="/" />
-            <div className="eventName event3">Event3</div>
+            <div className="eventName event3">• STCG</div>
           </div>
           <div className="sec4">
             <img className="eventImages" src={Event4} alt="/" />
-            <div className="eventName event4">Event4</div>
+            <div className="eventName event4">• Design Different</div>
           </div>
           <div className="sec5">
-            <img className="eventImages" src={Event4} alt="/" />
+            <img className="eventImages" src={Event5} alt="/" />
+            <div className="eventName event4">• Guest Lectures</div>
           </div>
           <div className="sec6">
-            <img className="eventImages" src={Event4} alt="/" />
+            <img className="eventImages" src={Event6} alt="/" />
+            <div className="eventName event4">• Panel Discussion</div>
           </div>
         </section>
       </div>
