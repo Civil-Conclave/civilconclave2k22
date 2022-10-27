b3c20b5c6764f03de829986b2c8f8da03044462f
ishaanjain047 <83901051+ishaanjain047@users.noreply.github.com>
2022-10-27 12:47:40 +0530

    about us animation fixed

--- src/components/AboutUs/Aboutusall.js
+++ src/components/AboutUs/Aboutusall.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./Aboutus.css";
 import data from "./data";
+import { motion, useScroll, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function Aboutusall() {
+  const control = useAnimation();
+  const { scrollYProgress } = useScroll();
+  const [lastRef, inView] = useInView();
+  const boxVariant = {
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: { duration: 0.5 },
+      scaleX: scrollYProgress,
+    },
+    hidden: { opacity: 0, scale: 0 },
+  };
+  useEffect(() => {
+    if (inView) {
+      console.log("View mein hai");
+      control.start("visible");
+    } else {
+      console.log("View mein nhi hai");
+      control.start("hidden");
+    }
+  }, [control, inView]);
   return (
     <>
       <div className="about-wrapper">
-        <div className="about-main-container">
+        <div className="about-main-container leftSide">
+          <motion.div
+            className="aboutBackground fillLeft"
+            style={{ scaleX: scrollYProgress }}
+          ></motion.div>
           <div className="about-context">
             <div className="about-heading-main">
               <h1 className="about-heading">{data[0].title}</h1>
@@ -17,7 +44,11 @@ export default function Aboutusall() {
             <img src={data[0].image} className="about-png"></img>
           </div>
         </div>
-        <div className="about-main-container">
+        <div className="about-main-container rightSide">
+          <motion.div
+            className="aboutBackground fillRight"
+            style={{ scaleX: scrollYProgress }}
+          ></motion.div>
           <div className="about-context">
             <div className="about-heading-main">
               <h1 className="about-heading">{data[1].title}</h1>
@@ -28,7 +59,11 @@ export default function Aboutusall() {
             <img src={data[1].image} className="about-png"></img>
           </div>
         </div>
-        <div className="about-main-container">
+        <div className="about-main-container leftSide" ref={lastRef}>
+          <motion.div
+            className="aboutBackground fillLeft"
+            style={{ scaleX: scrollYProgress }}
+          ></motion.div>
           <div className="about-context">
             <div className="about-heading-main">
               <h1 className="about-heading">{data[2].title}</h1>
