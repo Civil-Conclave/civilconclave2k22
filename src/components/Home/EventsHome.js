16c05a20a28a4cbb1bf1dcd62d195f98478df9ab
Prakhar Khatri <103585489+codeprakhar25@users.noreply.github.com>
2023-12-18 17:16:01 +0530

    Home Page fixes (#10)
    * fixed arrow shift
    
    * Updated Events (Home)
    
    ---------
    
    Co-authored-by: Dhruv Pankaj <dhruv.dpj@gmail.com>

--- src/components/Home/EventsHome.js
+++ src/components/Home/EventsHome.js
@@ -7,6 +7,7 @@ import Event5 from "../../assets/Event1.png";
 import Event6 from "../../assets/workshop.png";
 import Event7 from "../../assets/unsplash_gcsNOsPEXfs.png";
 import "./EventsHome.css";
+import {Link} from "react-router-dom";
 
 function Events() {
   const ref = useRef(null);
@@ -16,8 +17,8 @@ function Events() {
 
   useLayoutEffect(() => {
     setWidth(ref.current.offsetWidth);
-    console.log("uselayout triggered")
-    console.log("width:",width);
+    // console.log("uselayout triggered")
+    // console.log("width:",width);
   }, []);
 
 
@@ -27,29 +28,31 @@ function Events() {
   useEffect(() => {
     setPrevPos(0.05 * width);
     setNextPos(0.9 * width);
-    console.log("useeffect triggered")
+    // console.log("useeffect triggered")
   }, [width]);
 
   const scrollerPrev = (scrollOffset) => {
-    // console.log("prevpos is: ", prevPos)
+    console.log("prevpos is: ", prevPos)
     if (prevPos < 76) {
       return;
     }
     console.log(ref.current.scrollLeft);
     ref.current.scrollLeft += scrollOffset;
-    console.log("scrollLeft is", ref.current.scrollLeft);
+    // console.log("scrollLeft is", ref.current.scrollLeft);
     setPrevPos(prevPos - width / 4);
     setNextPos(nextPos - width / 4);
     setIsActive(true);
   };
   const scrollerNext = (scrollOffset) => {
-    if (nextPos === 1.4000000000000001 * width) {
+    // console.log("width",width)
+    // console.log("nextpos", nextPos)
+    if (nextPos/width>1.2) {
       return;
     }
     console.log(nextPos / width);
-    console.log(ref.current);
+    // console.log(ref.current);
     ref.current.scrollLeft += scrollOffset;
-    console.log("scrollLeft is", ref.current.scrollLeft);
+    // console.log("scrollLeft is", ref.current.scrollLeft);
     setPrevPos(prevPos + width / 4);
     setNextPos(nextPos + width / 4);
     setIsActive(true);
@@ -74,31 +77,31 @@ function Events() {
       <div className="eventHomeContainer">
         <div className="sec sec1">
           <img className="eventImages" src={Event1} alt="/" />
-          <div className="eventName event1"><span className="bullet">•</span> Case Study</div>
+          <div className="eventName event1"><Link style={{ color: 'white', textDecoration: 'none' }} to="/events"><span className="bullet">•</span> Case Study</Link></div>
         </div>
         <div className="sec 2">
           <img className="eventImages" src={Event2} alt="/" />
-          <div className="eventName event2"><span className="bullet">•</span> Research Renaissance</div>
+          <div className="eventName event2"><Link style={{color: 'white', textDecoration: 'none' }} to="/events"><span className="bullet">•</span> Research Renaissance</Link></div>
         </div>
         <div className="sec 3">
           <img className="eventImages" src={Event3} alt="/" />
-          <div className="eventName event3"><span className="bullet">•</span> Seek the civil geek</div>
+          <div className="eventName event3"><Link style={{ color: 'white',textDecoration: 'none' }} to="/events"><span className="bullet">•</span> Seek the civil geek</Link></div>
         </div>
         <div className="sec 4">
           <img className="eventImages" src={Event4} alt="/" />
-          <div className="eventName event4"><span className="bullet">•</span> Design Different</div>
+          <div className="eventName event4"><Link style={{color: 'white', textDecoration: 'none' }} to="/events"><span className="bullet">•</span> Design Different</Link></div>
         </div>
         <div className="sec 5">
           <img className="eventImages" src={Event5} alt="/" />
-          <div className="eventName event5"><span className="bullet">•</span> Guest Lectures</div>
+          <div className="eventName event5"><Link style={{color: 'white', textDecoration: 'none' }} to="/events"><span className="bullet">•</span> Guest Lectures</Link></div>
         </div>
         <div className="sec 6">
           <img className="eventImages" src={Event7} alt="/" />
-          <div className="eventName event6"><span className="bullet">•</span> Innovista</div>
+          <div className="eventName event6"><Link style={{ color: 'white',textDecoration: 'none' }} to="/events"><span className="bullet">•</span> Innovista</Link></div>
         </div>
         <div className="sec 7">
           <img className="eventImages" src={Event6} alt="/" />
-          <div className="eventName event7"><span className="bullet">•</span> Workshop</div>
+          <div className="eventName event7"><Link style={{color: 'white', textDecoration: 'none' }} to="/events"><span className="bullet">•</span> Workshop</Link></div>
         </div>
       </div>
     </div>
