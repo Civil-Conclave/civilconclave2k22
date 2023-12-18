188d6b44a46f9a90d9af85ec9c5f329029f58db1
183hardik <hardik_c@ce.iitr.ac.in>
2023-12-18 19:55:41 +0530

    about component in homepage and about us page changes

---
+++ src/components/Home/dataabout.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+      id: 1,
+      title: "VISION",
+      context: (
+        <p>
+        Civil Conclave 2021 provides a common platform for civil engineering enthusiasts. Being an inter-IIT event, it aims at bringing together the students from the most elite colleges of India to compete and network...
+        </p>
+      ),
+    
+    },
+    {
+      id: 2,
+      title: "CONCEPT",
+      context: (
+        <p>
+         On the whole, Civil Conclave 2021 is dedicated to nurturing technology and including youth power in civil engineering via different events. Different events like case studies, research work presentations and guest lectures...
+        </p>
+      ),
+   
+    },
+    {
+      id: 3,
+      title: "THEME",
+      context: (
+        <p>
+         Civil Conclave 2021 comes along with the theme of “Stimulating Sustainability”. Sustainability has always been the need of the hour. Keeping the growing technology and future needs in mind, adopting sustainable techniques...
+        </p>
+      ),
+
+     
+    },
+    {
+        id: 1,
+        title: "VISION",
+        context: (
+          <p>
+            Civil Conclave 2022 provides a common platform for civil engineering
+            enthusiasts.Being an inter-IIT event, it aims at bringing together the
+            students from the most elite colleges of India to compete and network.
+            <br></br>
+            <br></br>Young minds when put in the right direction can excel in all
+            fields. Civil engineering being the prime need of all times, rigorously
+            needs the budding minds for overall growth.<br></br>
+            <br></br>We understand this idea and are out to promote the same on the
+            inter-IIT level.
+          </p>
+        ),
+      
+      },
+  ];
+  export default data;
---
+++ src/components/Home/index.js
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import Landing from "./Landing.js";
+import EventsHome from "./EventsHome.js";
+import AboutConclave from "./AboutConclave.js";
+import Heading from "../General/Heading.js";
+import ContactUs from "../General/ContactUs.js";
+import Footer from "../General/Footer";
+import Sponsor from "./Sponsor/Sponsor.js";
+import AboutUS from "./AboutUS.js";
+import useScrollSnap from "react-use-scroll-snap";
+import {useRef} from "react";
+import Navbarnew from "../General/Navbar";
+
+
+function Home() {
+    const scrollRef = useRef(null)
+    useScrollSnap({ref: scrollRef, duration: 20, delay: 5});
+
+
+    return (
+        <div className="HomeWrapper" ref={scrollRef}>
+            <span>
+                <Navbarnew></Navbarnew>
+                <Landing></Landing>
+            </span>
+
+            <AboutConclave></AboutConclave>
+            <span>
+            <Heading name="Events"></Heading>
+            <EventsHome></EventsHome>
+            </span>
+            <Sponsor></Sponsor>
+            <AboutUS></AboutUS>
+            <ContactUs></ContactUs>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default Home;
